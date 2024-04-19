ef8df8c3b7fcf7933a3d5e03d372ae6aab533def
Bartek Janowski <BJanowskiiii@users.noreply.github.com>
2024-04-19 17:01:58 +0200

    Enhancement of UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,63 @@
-import './App.css';
-import {useEffect, useState} from "react";
+import { useEffect, useState } from 'react';
 
 function App() {
-    const [animals, setAnimals] = useState([]);
-
-    useEffect(() => {
-        const lastQuery = localStorage.getItem('last_query');
-        search(lastQuery)
-    }, []);
-
-    const search = async (q)=> {
-        const response = await fetch('http://localhost:8080?' + new URLSearchParams({q})
-        );
-        const data = await response.json()
-        setAnimals(data)
-
-        localStorage.setItem('lastQuery', q)
-    }
+    const { search, animals } = useAnimalSearch();
 
     return (
         <main style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
             <h1>Animal Farm</h1>
+
             <input
-                type='text'
-                placeholder='search'
-                onChange={(e)=>search(e.target.value)}
+                type="text"
+                placeholder="Search"
+                onChange={(e) => search(e.target.value)}
                 style={{ textAlign: 'center' }}
             />
 
             <ul style={{ listStyleType: 'none', padding: 0 }}>
-                {animals.map((animal)=>(
-                    <Animal key={animal.id}  {...animal} />
+                {animals.map((animal) => (
+                    <Animal key={animal.id} {...animal} />
                 ))}
-                {animals.length === 0 && 'No animal found, please type another letter to search animal :)'}
+
+                {animals.length === 0 && 'No animals found'}
             </ul>
         </main>
     );
 }
 
+// Dumb UI component
 // eslint-disable-next-line react/prop-types
-function Animal({type, age, name}){
+function Animal({ type, name, age }) {
     return (
         <li style={{ marginBottom: '5px', textAlign: 'center' }}>
-            <strong>{type}</strong>{name} ({age} years old)
+            <strong>{type}</strong> {name} ({age} years old)
         </li>
-    )
+    );
+}
+
+// Custom Hook
+function useAnimalSearch() {
+    const [animals, setAnimals] = useState([]);
+
+    useEffect(() => {
+        const lastQuery = localStorage.getItem('lastQuery');
+        search(lastQuery);
+    }, []);
+
+    const search = async (q) => {
+        const response = await fetch(
+            'http://localhost:8080?' + new URLSearchParams({ q })
+        );
+        const data = await response.json();
+        setAnimals(data);
+
+        localStorage.setItem('lastQuery', q);
+    };
+
+    return { search, animals };
 }
 
 export default App;
-
 
 
 // <li key={animal.id}>
